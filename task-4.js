5b1556ea55eddc33ca5e054120bc82c485211f64
hellosiyam <hellosiyambd@gmail.com>
2025-01-07 21:37:43 +0600

    Assignment-Task-4 complite

---
+++ task-4.js
@@ -0,0 +1,32 @@
+// function password(numbers) {
+//     let birthString = numbers.birthYear.toString();
+//     if (birthString.length < 4) {
+//         return "error"
+//     }
+    
+//     let nam = numbers.name;
+//     let birth = numbers.birthYear;
+//     let site = numbers.siteName;
+//     let sit=site[0].toUpperCase()+site.slice(1).toLowerCase();
+//     const thePassword =sit + "#" + nam + "@" + birth;
+//     return thePassword
+    
+// }
+// console.log(password({ name: "kolimuddin" , birthYear: 1020 , siteName: "google" }));
+
+function password(numbers) {
+    let strBirth = numbers.birthYear.toString();
+    if (strBirth.length < 4) {
+        return "Error";
+    }
+    const nam = numbers.name;
+    const birth = numbers.birthYear;
+    const site = numbers.siteName
+    const sit = site[0].toUpperCase()+site.slice(1).toLowerCase();
+    const thePassword = sit+ "#" + nam + "@" + birth;
+    return thePassword;
+    
+    
+}
+console.log(password({ name: "kolimuddin" , birthYear: 1099 , siteName: "google" }));
+
