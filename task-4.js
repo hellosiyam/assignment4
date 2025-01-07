ad2428b1fffd7053f6b3404e642d2f8b7a4c1521
hellosiyam <hellosiyambd@gmail.com>
2025-01-07 21:58:42 +0600

    Assignment-Task-4 complite

--- task-4.js
+++ task-4.js
@@ -1,34 +1,35 @@
-// function password(numbers) {
-//     let birthString = numbers.birthYear.toString();
-//     if (birthString.length < 4) {
-//         return "error"
-//     }
-    
-//     let nam = numbers.name;
-//     let birth = numbers.birthYear;
-//     let site = numbers.siteName;
-//     let sit=site[0].toUpperCase()+site.slice(1).toLowerCase();
-//     const thePassword =sit + "#" + nam + "@" + birth;
-//     return thePassword
-    
-// }
-// console.log(password({ name: "kolimuddin" , birthYear: 1020 , siteName: "google" }));
-
 function password(numbers) {
-    let number = Object.keys(numbers).length  
-    let strBirth = numbers.birthYear.toString();
-    if (strBirth.length < 4 || number < 3) {
-        return "Error";
+    let number = Object.keys(numbers).length;
+    let birthString = numbers.birthYear.toString();
+    if (birthString.length < 4 || number < 3) {
+        return "error"
     }
     
-    const nam = numbers.name;
-    const birth = numbers.birthYear;
-    const site = numbers.siteName
-    const sit = site[0].toUpperCase()+site.slice(1).toLowerCase();
-    const thePassword = sit+ "#" + nam + "@" + birth;
-    return thePassword;
-    
+    let nam = numbers.name;
+    let birth = numbers.birthYear;
+    let site = numbers.siteName;
+    let sit=site[0].toUpperCase()+site.slice(1).toLowerCase();
+    const thePassword =sit + "#" + nam + "@" + birth;
+    return thePassword
     
 }
-console.log(password({name: "kolimuddin"  , birthYear: 1099 , siteName: "google"}));
+console.log(password({ name: "kolimuddin" , birthYear: 1020 , siteName: "google" }));
+
+// function password(numbers) {
+//     let number = Object.keys(numbers).length  
+//     let strBirth = numbers.birthYear.toString();
+//     if (strBirth.length < 4 || number < 3) {
+//         return "Error";
+//     }
+    
+//     const nam = numbers.name;
+//     const birth = numbers.birthYear;
+//     const site = numbers.siteName
+//     const sit = site[0].toUpperCase()+site.slice(1).toLowerCase();
+//     const thePassword = sit+ "#" + nam + "@" + birth;
+//     return thePassword;
+    
+    
+// }
+// console.log(password({name: "kolimuddin"  , birthYear: 1099 , siteName: "google"}));
 
